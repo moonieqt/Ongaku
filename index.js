255adeebca893e7e35b2f975b3ccab7d69fbfa20
Pillo <kensei.dgok@gmail.com>
2021-01-17 02:47:19 +0000

    made everything pog

--- index.js
+++ index.js
@@ -1502,8 +1502,8 @@ if(command === "shop") {
             message.channel.send(new Ongaku.MessageEmbed()
     .setColor("RANDOM")
        .setAuthor(client.user.username, client.user.displayAvatarURL())
-       .setTitle(`Welcome to ${message.guild.name}'s Shop`)
-      .setDescription(`${client.user.name}'s Global Shop Items\n\n**Weapons and Ammo**\n---------------\n| Pistol: \`240$\`\n| Ak-14: \`340\`\n| M14: \`500$\`\n| RPG: \`2390$\`\n| ABAT Catapillar 149: \`52003$\`\n---------------\nPistol rounds: \`30$\`\nAk rounds: \`56$\`\nM14 rounds: \`120$\`\nRPG rockets: \`300$\`\nABAT rounds: \`4000$\`\n\n**Pets and Food**\n---------------\nKaiou: \`130$\`\nBonzo: \`330$\`\nNeiphi: \`100$\`\nBowo:\`1600$\`\nChaoi: \`1020\`\n---------------\nSeeds: \`10$\`\nMeat: \`30$\`\nBait: \`20$\`\nRice: \`60$\`\nGM carrots: \`100$\`\n\n**Badges**\n---------------\nBronze I: \`10000$\`\nBronze II: \`15000$\`\nBronze III: \`20000$\``)
+       .setTitle(`Welcome to ${client.user.username}'s Shop`)
+      .setDescription(`${client.user.username}'s Global Shop Items\n\n**Weapons and Ammo**\n---------------\n| Pistol: \`240$\`\n| Ak-14: \`340\`\n| M14: \`500$\`\n| RPG: \`2390$\`\n| ABAT Catapillar 149: \`52003$\`\n---------------\nPistol rounds: \`30$\`\nAk rounds: \`56$\`\nM14 rounds: \`120$\`\nRPG rockets: \`300$\`\nABAT rounds: \`4000$\`\n\n**Pets and Food**\n---------------\nKaiou: \`130$\`\nBonzo: \`330$\`\nNeiphi: \`100$\`\nBowo:\`1600$\`\nChaoi: \`1020\`\n---------------\nSeeds: \`10$\`\nMeat: \`30$\`\nBait: \`20$\`\nRice: \`60$\`\nGM carrots: \`100$\`\n\n**Badges**\n---------------\nBronze I: \`10000$\`\nBronze II: \`15000$\`\nBronze III: \`20000$\``)
       )
             } else {
             message.channel.send(embed)
@@ -1964,54 +1964,129 @@ if(command === "shop") {
         //black jack commands
         if(command === "userinfo") {
 
-        if(args.length > 1) return message.channel.send('Only mention one user!');
-
-    //check if there is 1 argument
-    if(args[0]){
-      //get the first user mentioned
-      let member = message.mentions.members.first() || message.author;
-
-      //if the member exists create an embed with info about that user and send it to the channel
-      if(member) {
-        let embed = new Ongaku.MessageEmbed()
-          .setColor("RANDOM")
-          .setTitle("User Info")
-          .setThumbnail(member.user.displayAvatarURL())
-          .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
-          .addField("**Username:**", `${member.user.username}`)
-          .addField("**Discriminator:**", `${member.user.discriminator}`)
-          .addField("**ID:**", `${member.user.id}`)
-          .addField("**Status:**", `${member.user.presence.status}`)
-          .addField("**Joined On:**", `${member.joinedAt.toLocaleString()}`)
-          .addField("**Created On:**", `${member.user.createdAt.toLocaleString()}`)
-          .addField("**All Roles:**", `${member.roles.cache.map(role => role.toString()).join(' ')}`)
-          .setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
-
-        message.channel.send(embed);
-      } else {
-          message.channel.send(`Could not find that member`); //send a message to the channel if the user doesn't exist
-      }
+        const status = {
+    online: "Online",
+    idle: "Idle",
+    dnd: "Do Not Disturb",
+    offline: "Offline/Invisible"
+      };
+       var permissions = [];
+    var acknowledgements = 'None';
+   
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+    const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); }); 
+    
+    if(message.member.hasPermission("KICK_MEMBERS")){
+        permissions.push("Kick Members");
     }
+    
+    if(message.member.hasPermission("BAN_MEMBERS")){
+        permissions.push("Ban Members");
+    }
+    
+    if(message.member.hasPermission("ADMINISTRATOR")){
+        permissions.push("Administrator");
+    }
+
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+        permissions.push("Manage Messages");
+    }
+    
+    if(message.member.hasPermission("MANAGE_CHANNELS")){
+        permissions.push("Manage Channels");
+    }
+    
+    if(message.member.hasPermission("MENTION_EVERYONE")){
+        permissions.push("Mention Everyone");
+    }
+
+    if(message.member.hasPermission("MANAGE_NICKNAMES")){
+        permissions.push("Manage Nicknames");
+    }
+
+    if(message.member.hasPermission("MANAGE_ROLES")){
+        permissions.push("Manage Roles");
+    }
+
+    if(message.member.hasPermission("MANAGE_WEBHOOKS")){
+        permissions.push("Manage Webhooks");
+    }
+
+    if(message.member.hasPermission("MANAGE_EMOJIS")){
+        permissions.push("Manage Emojis");
+    }
+
+    if(permissions.length == 0){
+        permissions.push("No Key Permissions Found");
+    }
+
+    if(member.user.id == message.guild.ownerID){
+        acknowledgements = 'Server Owner';
+    }
+
+    const embed = new Ongaku.MessageEmbed()
+        .setDescription(`<@${member.user.id}>`)
+        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
+        .setColor(randomColor)
+        .setFooter(`ID: ${message.author.id}`)
+        .setThumbnail(member.user.displayAvatarURL)
+        .setTimestamp()
+        .addField("Status",`${status[member.user.presence.status]}`, true)
+        .addField('Joined at: ',`${moment(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`)
+        .addField("Created at: ",`${moment(message.author.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
+        .addField("Permissions: ", `${permissions.join(', ')}`)
+        .addField(`Roles [${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).length}]`,`${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `<@&${roles.id }>`).join(" **|** ") || "No Roles"}`, true)
+        .addField("Acknowledgements: ", `${acknowledgements}`)
+          .addField("**Status:**", `${member.user.presence.status}`);
+        
+    message.channel.send({embed});
         }
 
     if(command === "serverinfo") {
-         let embed = new Ongaku.MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("Server Info")
-      .setThumbnail(message.guild.iconURL())
-      .setAuthor(`${message.guild.name}`, message.guild.iconURL())
-      .addField("**Guild Owner:**", `${message.guild.owner}`)
-      .addField("**Member Count:**", `${message.guild.memberCount}`)
-      .addField("**Total Real Members**", message.guild.members.cache.filter(member => !member.user.bot).size)
-      .addField("**Total Bots**", message.guild.members.cache.filter(member => member.user.bot).size)
-      .addField("**Total Channels**", message.guild.channels.cache.size)
-      .addField("**Total Text Channels**", message.guild.channels.cache.filter(ch => ch.type === 'text').size)
-      .addField("**Total Voice Channels**", message.guild.channels.cache.filter(ch => ch.type === 'voice').size)
-      .addField("**Created On**", message.guild.createdAt.toLocaleString())
-      .addField("**All roles**", `${message.guild.roles.cache.map(role => role.toString()).join(' ')}`)
-      .setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
+         function checkBots(guild) {
+        let botCount = 0;
+        guild.members.cache.forEach(member => {
+            if(member.user.bot) botCount++;
+        });
+        return botCount;
+    }
     
-    message.channel.send(embed);
+    function checkMembers(guild) {
+        let memberCount = 0;
+        guild.members.cache.forEach(member => {
+            if(!member.user.bot) memberCount++;
+        });
+        return memberCount;
+    }
+
+    function checkOnlineUsers(guild) {
+        let onlineCount = 0;
+        guild.members.cache.forEach(member => {
+            if(member.user.presence.status === "online")
+                onlineCount++; 
+        });
+        return onlineCount;
+    }
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Ongaku.MessageEmbed()
+        .setAuthor(`${message.guild.name} - Informations`, message.guild.iconURL)
+        .setColor("RANDOM")
+        .setThumbnail(message.guild.iconURL())
+        .addField('Server owner', message.guild.owner, true)
+        .addField('Server region', message.guild.region, true)
+        .addField("Server Name", message.guild.name)
+        .addField('Verification level', message.guild.verificationLevel, true)
+        .addField('Channel count', message.guild.channels.cache.size, true)
+        .addField('Total member count', message.guild.memberCount)
+        .addField('Humans', checkMembers(message.guild), true)
+        .addField('Bots', checkBots(message.guild), true)
+        .addField('Online', checkOnlineUsers(message.guild))
+      .addField("**All roles**", `${message.guild.roles.cache.map(role => role.toString()).join(' ')}`)
+        .setFooter('Guild created at:')
+        .setTimestamp(message.guild.createdAt);
+
+    return message.channel.send(serverembed);
     }
 
     //end of info commmands
